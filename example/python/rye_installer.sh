f4e397da0fb42f44e4e5a69a7333883689d1fa06
cyokozai <yuu4suke27@outlook.jp>
2024-04-09 12:48:25 +0900

    20240409

---
+++ example/python/rye_installer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Install Rye
+curl -sSf https://rye-up.com/get | bash &&\
+echo 'source "$HOME/.rye/env"' >> ~/.profile &&\
+echo 'source "$HOME/.rye/env"' >> ~/.bashrc &&\
+source ~/.bashrc &&\
+rye self completion > ~/.local/share/bash-completion/completions/rye.bash
+rye self update 
+rm -f rye_installer.sh
